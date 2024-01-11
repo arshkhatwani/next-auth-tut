47ae14c1b4b9c22653447b24db90df0ef81514d3
arshkhatwani <arsh.khatwani@gmail.com>
2024-01-11 23:26:10 +0530

    Adds CredentialsProvider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,7 @@
-import NextAuth from "next-auth";
+import NextAuth, { User } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import users from "@/app/constants/users";
 
 export const authOptions = {
     providers: [
@@ -7,8 +9,31 @@ export const authOptions = {
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
         }),
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {
+                    label: "Email",
+                    type: "text",
+                    placeholder: "Enter your email",
+                },
+                password: { label: "Password", type: "password" },
+            },
+            async authorize(credentials) {
+                const email = credentials?.email;
+                const password = credentials?.password;
+
+                if (!email || !password) return null;
+
+                const user = users.find((e) => e.email === email);
+                if (user?.password === password)
+                    return { id: user.id, email: user.email, name: user.name };
+
+                return null;
+            },
+        }),
     ],
-    secret: process.env.NEXTAUTH_SECRET as string
+    secret: process.env.NEXTAUTH_SECRET as string,
 };
 
 const handler = NextAuth(authOptions);
