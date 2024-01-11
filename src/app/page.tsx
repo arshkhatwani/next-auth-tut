2baa17e0cf685adfa1db6d4b672ba779f0ae1e6a
arshkhatwani <arsh.khatwani@gmail.com>
2024-01-12 00:01:31 +0530

    Adds navbar and authprovider to app

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,19 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
+import Navbar from "./components/Navbar";
 
 export default async function Home() {
     const session = await getServerSession(authOptions);
     const user = session?.user;
 
     return (
-        <div className="p-5">
-            <h1 className="text-3xl">Hello, {user?.name}</h1>
-        </div>
+        <>
+            <Navbar />
+            <div className="p-5">
+                <h1 className="text-3xl">
+                    Hello, {user?.name}. Your email is {user?.email}
+                </h1>
+            </div>
+        </>
     );
 }
