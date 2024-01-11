a7e40e9cf0b91cc8e206ec613bfaed0cd00f7c06
arshkhatwani <arsh.khatwani@gmail.com>
2024-01-11 23:28:55 +0530

    Adds logic to fetch user details

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-    return <div>Hello world</div>;
+import { getServerSession } from "next-auth";
+import { authOptions } from "./api/auth/[...nextauth]/route";
+
+export default async function Home() {
+    const session = await getServerSession(authOptions);
+    const user = session?.user;
+
+    return (
+        <div className="p-5">
+            <h1 className="text-3xl">Hello, {user?.name}</h1>
+        </div>
+    );
 }
