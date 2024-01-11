fb779803e706e94bda589e686526953c506fe87a
arshkhatwani <arsh.khatwani@gmail.com>
2024-01-12 00:07:58 +0530

    Adds redirect in home screen

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,15 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "./api/auth/[...nextauth]/route";
 import Navbar from "./components/Navbar";
+import { redirect } from "next/navigation";
 
 export default async function Home() {
     const session = await getServerSession(authOptions);
     const user = session?.user;
 
+    if (!session) {
+        redirect("/api/auth/signin");
+    }
     return (
         <>
             <Navbar />
