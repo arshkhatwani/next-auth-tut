f121d916df0eb985457d3f754c273913dae84030
arshkhatwani <arsh.khatwani@gmail.com>
2024-01-14 13:03:30 +0530

    Update google provider and disable creds provider

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,39 +1,52 @@
-import NextAuth from "next-auth";
+import NextAuth, { AuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import users from "@/app/constants/users";
 
-export const authOptions = {
+export const authOptions: AuthOptions = {
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-        CredentialsProvider({
-            name: "Credentials",
-            credentials: {
-                email: {
-                    label: "Email",
-                    type: "text",
-                    placeholder: "Enter your email",
+            authorization: {
+                params: {
+                    prompt: "consent",
+                    access_type: "offline",
+                    response_type: "code",
                 },
-                password: { label: "Password", type: "password" },
-            },
-            async authorize(credentials) {
-                const email = credentials?.email;
-                const password = credentials?.password;
-
-                if (!email || !password) return null;
-
-                const user = users.find((e) => e.email === email);
-                if (user?.password === password)
-                    return { id: user.id, email: user.email, name: user.name };
-
-                return null;
             },
         }),
+        // CredentialsProvider({
+        //     name: "Credentials",
+        //     credentials: {
+        //         email: {
+        //             label: "Email",
+        //             type: "text",
+        //             placeholder: "Enter your email",
+        //         },
+        //         password: { label: "Password", type: "password" },
+        //     },
+        //     async authorize(credentials) {
+        //         const email = credentials?.email;
+        //         const password = credentials?.password;
+
+        //         if (!email || !password) return null;
+
+        //         const user = users.find((e) => e.email === email);
+        //         if (user?.password === password)
+        //             return { id: user.id, email: user.email, name: user.name };
+
+        //         return null;
+        //     },
+        // }),
     ],
     secret: process.env.NEXTAUTH_SECRET as string,
+    callbacks: {
+        async session({ session, user, token }) {
+            // This is callback
+            return session;
+        },
+    },
 };
 
 const handler = NextAuth(authOptions);
